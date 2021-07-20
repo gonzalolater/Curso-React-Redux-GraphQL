fb72a2a642394b147d08acc1ba867eee4eb2933b
gonzalolater <gonzalolater>
2021-07-20 12:22:02 -0300

    Agregando duck al store y despachando nuestra primera accion

---
+++ src/redux/store.js
@@ -0,0 +1,21 @@
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
+import userReducer from './userDuck'
+import charsReducer, {getCharactersAction} from './charsDucks'
+import thunk from 'redux-thunk'
+
+let rootReducer = combineReducers({
+    user:userReducer,
+    characters:charsReducer
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function generateStore(){
+    let store = createStore(
+        rootReducer,
+        composeEnhancers(applyMiddleware(thunk))
+    )
+    // consiguiendo los personajes por primera vez
+    getCharactersAction()(store.dispatch, store.getState)
+    return store
+}
